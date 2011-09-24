90fc57b923ce8aecc2778c6d9d443d4b44bfefca
Martin Traverso <mtraverso@acm.org>
2011-09-23 21:19:56 -0700

    process the main artifact of a project in addition to attachments
    Also, only touch artifacts of type "jar" and report an error
    if no artifacts were found

--- src/main/java/org/skife/waffles/MyMojo.java
+++ src/main/java/org/skife/waffles/MyMojo.java
@@ -11,6 +11,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @goal really-executable-jar
@@ -37,26 +39,52 @@ public class MyMojo extends AbstractMojo
     public void execute() throws MojoExecutionException
     {
         try {
+            List<File> files = new ArrayList<File>();
 
-            for (Object o : project.getAttachedArtifacts()) {
-                AttachedArtifact a = (AttachedArtifact) o;
-                File f = a.getFile();
-                File tmp = File.createTempFile("waffles", ".tmp");
+            if (project.getArtifact() != null && project.getArtifact().getType().equals("jar")) {
+                files.add(project.getArtifact().getFile());
+            }
 
-                FileUtils.rename(f, tmp);
+            for (Object item : project.getAttachedArtifacts()) {
+                AttachedArtifact artifact = (AttachedArtifact) item;
+                if (artifact.getType().equals("jar")) {
+                    files.add(artifact.getFile());
+                }
+            }
 
-                FileOutputStream fout = new FileOutputStream(f);
-                FileInputStream in = new FileInputStream(tmp);
-                fout.write(("#!/bin/sh\n\nexec java " + flags + " -jar $0 \"$@\"\n\n").getBytes());
-                IOUtil.copy(in, fout);
-                in.close();
-                fout.close();
-                Runtime.getRuntime().exec("chmod +x " + f.getAbsolutePath());
+            if (files.isEmpty()) {
+                throw new MojoExecutionException("Could not find any jars to make executable");
+            }
+
+            for (File file : files) {
+                makeExecutable(file);
             }
         }
         catch (IOException e) {
             throw new MojoExecutionException(e, "FAILURE!", e.getMessage());
         }
 
+    }
+
+    private void makeExecutable(File file)
+            throws IOException
+    {
+        getLog().debug("Making " + file.getAbsolutePath() + " executable");
+
+        File tmp = File.createTempFile("waffles", ".tmp");
+        try {
+            FileUtils.rename(file, tmp);
+
+            FileOutputStream out = new FileOutputStream(file);
+            FileInputStream in = new FileInputStream(tmp);
+            out.write(("#!/bin/sh\n\nexec java " + flags + " -jar $0 \"$@\"\n\n").getBytes("ASCII"));
+            IOUtil.copy(in, out);
+            in.close();
+            out.close();
+            Runtime.getRuntime().exec("chmod +x " + file.getAbsolutePath());
+        }
+        finally {
+            tmp.delete();
+        }
     }
 }
