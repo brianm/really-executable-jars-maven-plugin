d66fa82ec2938b8eed9b374f43fb4f0e164ef0e3
Klaus Brunner <klaus.brunner@gmail.com>
2023-02-19 09:46:34 +0100

    fix: Java 8 compatibility of test app

--- src/it/resources/test-project/src/main/java/org/test/App.java
+++ src/it/resources/test-project/src/main/java/org/test/App.java
@@ -1,10 +1,11 @@
 package org.test;
 
-import java.util.Objects;
-
 public class App {
     public static void main(String[] args) {
-        String salutation = Objects.requireNonNullElse(System.getProperty("app.greeting"), "Hello");
+        String salutation = System.getProperty("app.greeting");
+        if (salutation == null) {
+            salutation = "Hello";
+        }
         String addressee = args.length > 0 ? args[0] : "world";
 
         System.out.printf("%s %s!%n", salutation, addressee);
